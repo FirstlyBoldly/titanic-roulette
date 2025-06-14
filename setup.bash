87856e67f58f85902ad870fdc11df37e302f52b8
FirstlyBoldly <ankhbhold@gmail.com>
2025-06-14 22:14:20 +0900

    Added animations and sound effects.

--- setup.bash
+++ setup.bash
@@ -1,5 +1,4 @@
-# gcloud auth login --no-browser
-cd server/src/
+cd server/
 gcloud builds submit --tag gcr.io/titanicroulette/flask-app
 gcloud run deploy flask-app --image gcr.io/titanicroulette/flask-app --platform managed --region us-central1 --allow-unauthenticated
 firebase deploy
